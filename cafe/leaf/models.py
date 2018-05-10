1e7457618e992bb65bb0fd906aec715043d0fd9a
김하람 <gkfka5968@naver.com>
2018-05-10 20:57:47 +0900

    add User,Tag model add data

--- cafe/leaf/models.py
+++ cafe/leaf/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    id=models.IntegerField(primary_key=True)
+    user_id=models.CharField(max_length=20)
+    password=models.CharField(max_length=20)
+    name=models.CharField(max_length=10)
+    is_staff=models.BooleanField(default=True)
+    tag_search=models.CharField(max_length=1000)
+    cafe_id=models.IntegerField(default=0)
+
+class Tag(models.Model):
+    id=models.IntegerField(primary_key=True,foreign_key=True)
+    name=models.CharField(max_length=45)
+
