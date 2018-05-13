e486f4ecade292267b4e7c2beb3111c3ecc9efdd
KwakSeungeun <kseun0410@gmail.com>
2018-05-13 23:57:21 +0900

    Modify Tag, User models

--- cafe/leaf/models.py
+++ cafe/leaf/models.py
@@ -1,25 +1,25 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
-    id=models.IntegerField(primary_key=True)
-    user_id=models.CharField(max_length=20)
-    password=models.CharField(max_length=20)
-    name=models.CharField(max_length=10)
-    is_staff=models.BooleanField(default=True)
-    tag_search=models.CharField(max_length=1000)
-    cafe_id=models.IntegerField()
-
-class Tag(models.Model):
-    id=models.IntegerField(primary_key=True,foreign_key=True)
-    name=models.CharField(max_length=45)
-
 class Cafe(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=45)
     location = models.CharField(max_length=100)
     available_seat = models.IntegerField()
     tag_id = models.IntegerField()
+
+class Tag(models.Model):
+    id = models.ForeignKey(Cafe, primary_key=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=45)
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    name = models.CharField(max_length=10)
+    is_staff = models.BooleanField(default=False)
+    tag_search = models.CharField(max_length=1000, null=True)
+    cafe_id = models.ForeignKey(Cafe, on_delete=models.CASCADE, null=True)
 
 class Comment(models.Model):
     id = models.AutoField(primary_key=True)
--- cafe/leaf/urls.py
+++ cafe/leaf/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('orange/', views.main),
+    path('cafe/', views.main),
 ]
