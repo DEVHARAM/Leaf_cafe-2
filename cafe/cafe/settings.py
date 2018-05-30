8d448062cd0f08ddb7826c97f6eaf8a55e8f6614
Ryunhee Han <ryuneeee@gmail.com>
2018-05-30 12:11:07 +0900

    Change database to remote

--- cafe/cafe/settings.py
+++ cafe/cafe/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'cafe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'leaf',
+        'USER': 'root',
+        'PASSWORD': 'J2qoDOXADNV6DVVqsVe1NOiLY1JzhL',
+        'HOST': 'dev.ryuneeee.com',
+        'PORT': '3306',
     }
 }
 
