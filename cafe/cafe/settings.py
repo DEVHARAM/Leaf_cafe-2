d11df033e2c8e4bbd75a7693c255c0637b0ec17a
Ryunhee Han <ryuneeee@gmail.com>
2018-06-01 04:14:01 +0900

    1. Repair Django-Admin site 2. Remove url prefix 3. Add marker into map (with InfoWindow event) 4. Add jQuery

--- cafe/cafe/settings.py
+++ cafe/cafe/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'cafe.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR],
+        'DIRS': [os.path.join(BASE_DIR, 'leaf/templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
