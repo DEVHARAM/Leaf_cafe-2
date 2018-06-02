7dcf1720ba9ba26d02971cc16a414c8d27bc65d9
Ryunhee Han <ryuneeee@gmail.com>
2018-06-03 05:10:34 +0900

    1. Tags ManyToMany로 변경 2. Searching 적용 3. 마이너 디자인 변경 4. Tags 적용 (Ajax)

--- cafe/leaf/views.py
+++ cafe/leaf/views.py
@@ -1,8 +1,24 @@
+import json
+
 from django.core import serializers
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import *
 
+
 def main(request):
-    cafe_data = serializers.serialize('json', Cafe.objects.all())
+    if 'tag' in request.GET:
+        try:
+            cafes = Tag.objects.filter(name=request.GET['tag']).first().cafe_set.all()
+        except:
+            cafes = []
+    else:
+        cafes = Cafe.objects.all()
+
+    cafe_data = serializers.serialize('json', cafes, fields=('name', 'location', 'available_seat'))
     return render(request, 'main.html', {"cafe_data": cafe_data})
 
+
+def cafe_detail(request, cafe_id):
+    tags = [t.name for t in Cafe.objects.filter(id=cafe_id).first().tags.all()]
+    return HttpResponse(json.dumps(tags), content_type='application/json')
