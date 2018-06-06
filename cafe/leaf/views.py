fb56660549add06f52f9c158135eaac1ad42990a
Ryunhee Han <ryuneeee@gmail.com>
2018-06-07 01:55:45 +0900

    Applying recent search

--- cafe/leaf/views.py
+++ cafe/leaf/views.py
@@ -8,12 +8,24 @@ from .models import *
 
 def main(request):
     user = None
+    recent = []
     if 'current_user' in request.session:
         user = User.getUser(request.session['current_user'])
+        if user.tag_search:
+            recent = user.tag_search.split('|')
 
-    cafes = Cafe.getCafesByTag(request.GET.get('tag'))
+    tag = request.GET.get('tag')
+    if tag and user:
+        recent = user.tag_search.split('|')
+        recent.append(tag)
+        if len(recent) > 5:
+            recent = recent[-5:]
+        user.tag_search = '|'.join(recent)
+        user.save()
+
+    cafes = Cafe.getCafesByTag(tag)
     cafe_data = serializers.serialize('json', cafes, fields=('name', 'location', 'available_seat'))
-    return render(request, 'main.html', {"cafe_data": cafe_data, 'user': user, 'tags':Tag.objects.all()})
+    return render(request, 'main.html', {"cafe_data": cafe_data, 'user': user, 'tags':Tag.objects.all(), 'recent': recent})
 
 
 def cafe_detail(request, cafe_id):
